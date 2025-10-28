ceb142e0af1061239d1df0b7c34d84d1f53a06f2
ogustavofriasx <gusvfrias15@gmail.com>
2025-10-28 18:21:08 -0300

    CORS

--- api/main.py
+++ api/main.py
@@ -4,9 +4,22 @@ from .routers.jogador_route import router as jogadores_router
 from .routers.partida_route import router as partidas_router  
 from .routers.ranking_route import router as ranking_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#CORS
+origins = ["https://api-marvel-rivals.onrender.com", "*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+#Routes
 app.include_router(personagens_router)
 app.include_router(jogadores_router)
 app.include_router(partidas_router)
